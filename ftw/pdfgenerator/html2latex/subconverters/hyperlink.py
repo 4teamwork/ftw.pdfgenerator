51cf57dc4261cfd439d47bf986288569365aa44e
Jonas Baumann <jone@jone.ch>
2012-06-07 18:06:25 +0200

    Fix: "Hyperlinks: handle underscores and hash keys in urls."

--- ftw/pdfgenerator/html2latex/subconverters/hyperlink.py
+++ ftw/pdfgenerator/html2latex/subconverters/hyperlink.py
@@ -22,7 +22,7 @@ class HyperlinkConverter(subconverter.SubConverter):
 
         url = url.replace('&amp;', '&')
         url = url.replace(' ', '%20').replace('%', '\%')
-        url = url.replace('_', '\_').url.replace('#', '\#')
+        url = url.replace('_', '\_').replace('#', '\#')
 
         self.get_layout().use_package('hyperref')
         self.replace_and_lock(self.latex_link(url, label))
