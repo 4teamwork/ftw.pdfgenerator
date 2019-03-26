372fbbcf517ce4de1d06d2464f89bae60733558f
Joni Orponen <j.orponen@4teamwork.ch>
2019-03-26 15:13:02 +0100

    Robustify URL conversion.

--- ftw/pdfgenerator/html2latex/subconverters/url.py
+++ ftw/pdfgenerator/html2latex/subconverters/url.py
@@ -1,15 +1,15 @@
 from ftw.pdfgenerator.html2latex import subconverter
-from ftw.pdfgenerator.interfaces import HTML2LATEX_PREVENT_CHARACTER
-from Products.CMFCore.utils import getToolByName
 from urlparse import urlparse
 from urlparse import urlunparse
-import os.path
-import re
 
 
 class URLConverter(subconverter.SubConverter):
-    """Converts URLs within text (not link tags) so that they
-    support hyphenation.
+    """Convert URLs within text (not link tags).
+
+    * Add hyphenation hints onto path segments
+    * Add hyphenation hints onto query string parametres
+    * Convert xml entities from minidom `element.toxml()`
+    * Escape latex nasties for link tags
     """
 
     # http://stackoverflow.com/questions/6883049/regex-to-find-urls-in-string-in-python
@@ -18,7 +18,25 @@ class URLConverter(subconverter.SubConverter):
     def __call__(self):
         url = self.get_html()
         scheme, netloc, path, params, query, fragment = urlparse(url)
-        path = path.replace('/', '""/')
+
+        # Make path segments hyphenable
+        path = '""/'.join(path.split('/'))
+
+        # Handle minidom `element.toxml()`
+        query = query.replace('&amp;', '&')
+
+        # Make query string parametres hyphenable
+        query = query.replace('&', '""&')
+
         url = urlunparse((scheme, netloc, path, params, query, fragment))
+
+        # Work around urlunparse() treating the hyphenation hint as a segment
         url = url.replace('/""/', '""/', 1)
+
+        # Replace latex nasties for conversion
+        url = url.replace('_', r'\_')
+        url = url.replace('&', r'\&')
+        url = url.replace('#', r'\#')
+        url = url.replace('%', r'\%')
+
         self.replace_and_lock(url)
